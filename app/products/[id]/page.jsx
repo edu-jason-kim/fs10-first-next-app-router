aac079ac43661d7fd44574f62585eb976f8cc12a
edu-jason-kim <edu.jason.kim@gmail.com>
2025-12-10 11:38:48 +0900

    server component vs client component

---
+++ app/products/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default function ProductDetailPage() {
+  return (
+    <main>
+      <h1>프로덕트 디테일 페이지</h1>
+    </main>
+  );
+}
---
+++ app/products/page.jsx
@@ -0,0 +1,24 @@
+import Counter from "./components/Counter";
+
+// 서버에서만 실행하는 컴포넌트 -> 목적이 서버에서 HTML만드는 것
+export default async function ProductsPage() {
+  const response = await fetch(
+    "https://learn.codeit.kr/api/codeitmall/products"
+  );
+  const data = await response.json();
+  const products = data.results;
+
+  return (
+    <main>
+      <h1>프로덕트 페이지</h1>
+      <hr />
+      <Counter />
+      <hr />
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
