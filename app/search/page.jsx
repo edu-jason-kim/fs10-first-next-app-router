7b5a0c4813a2c4f4b64b930cb51786dc53ce12a0
edu-jason-kim <edu.jason.kim@gmail.com>
2025-12-10 13:46:47 +0900

    search page

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,16 +1,19 @@
 import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 
-export default async function Home() {
+export default async function SearchPage({ searchParams }) {
+  // 서버에서 쿼리 스트링 접근
+  const { q } = await searchParams;
+
   const response = await fetch(
-    "https://learn.codeit.kr/api/codeitmall/products"
+    "https://learn.codeit.kr/api/codeitmall/products" + `?q=${q}`
   );
   const data = await response.json();
   const products = data.results;
 
   return (
     <main>
-      <h1>홈페이지</h1>
+      <h1>검색결과 페이지: {q}</h1>
       <SearchForm />
       <ProductList products={products} />
     </main>
